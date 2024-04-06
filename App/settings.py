67978be61aef8dd09694b6a9fe0f8d0a5d35190a
Amit Kumar <amitraazec53@gmail.com>
2024-04-06 13:34:50 +0530

    ch

--- App/settings.py
+++ App/settings.py
@@ -158,33 +158,35 @@ WSGI_APPLICATION = "App.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# # settings.py
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Vijay_johar_db',  # Replace 'your_database_name' with the name of your MySQL database
-#         'USER': '2024vijaydb',  # Replace 'your_database_username' with the MySQL username
-#         'PASSWORD': 'Vijay12345@#$',  # Replace 'your_database_password' with the MySQL password
-#         'HOST': 'localhost',  # MySQL host (usually 'localhost' for Hostinger)
-#         'PORT': '3306',  # MySQL port (usually '3306' for Hostinger)
-       
+#         'NAME': 'vijay_db',       # Replace 'your_database_name' with your database name
+#         'USER': 'vijay_82',   # Replace 'your_database_username' with your database username
+#         'PASSWORD': 'Coach@AWS82',  # Replace 'your_database_password' with your database password
+#         'HOST': '127.0.0.1',          # Replace 'your_mysql_host' with your MySQL host (e.g., 'localhost')
+#         'PORT': '3306',                     # MySQL port (usually '3306')
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Anshu',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Anshu',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
