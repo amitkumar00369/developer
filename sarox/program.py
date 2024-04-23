1015324f8255f6b9bc52e0caf819183c2d762ccf
Amit Kumar <amitraazec53@gmail.com>
2024-04-23 18:40:23 +0530

    ch

--- sarox/program.py
+++ sarox/program.py
@@ -505,7 +505,7 @@ class GetAssignCourseofCoach(APIView):
             
                
 
-                return Response({'message': 'Get all course of coach', 'data': course_list,'name':set(courses_name_list),'status': status.HTTP_200_OK},status.HTTP_200_OK)
+                return Response({'message': 'Get all course of coach', 'data': course_list,'course_name':set(courses_name_list),'status': status.HTTP_200_OK},status.HTTP_200_OK)
       
         except user.DoesNotExist:
             return Response({'error':'User not found','status':status.HTTP_500_INTERNAL_SERVER_ERROR},status.HTTP_500_INTERNAL_SERVER_ERROR)
