06bf6c0d3af28d8795b235cbbafbe6832342a0c8
Amit Kumar <amitraazec53@gmail.com>
2024-05-09 11:37:30 +0530

    ch

--- sarox/urls.py
+++ sarox/urls.py
@@ -8,8 +8,8 @@ from .mail import sendMail,videoUpload,postThought,getAllVideo,getAllThoughts,de
 from .program import CreateallProgram,getAllProgram,deleteProgram,updateProgram,AssignCourseByemail,GetAssignCourseofCoach,activeCourse,archiveCourse
 from .archive import archiveCourses,CoachDashboard
 
-from .google_api import GenerateForm
-from .api import CreateGoogleForm
+# from .google_api import GenerateForm
+# from .api import CreateGoogleForm
 from .views import ResetPassword
 
 
@@ -110,8 +110,8 @@ urlpatterns = [
     path('CoachDashboard/<str:email>',CoachDashboard.as_view()),
     path('CoachDashboard/',CoachDashboard.as_view()),
     
-    path('googleform',GenerateForm.as_view()),
-    path('CreateGoogleForm',CreateGoogleForm.as_view()),
+    # path('googleform',GenerateForm.as_view()),
+    # path('CreateGoogleForm',CreateGoogleForm.as_view()),
     
     
     
